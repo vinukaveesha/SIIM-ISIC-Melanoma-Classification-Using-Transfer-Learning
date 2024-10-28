b93e08e88e178312b57e58f3f966ab6fa835a3ec
Vinu Kaveesha De Zoysa <vinukaveesha2001@gmail.com>
2024-10-28 11:44:09 +0530

    kn

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ from utils.scheduler import create_early_stopping
 from utils.preprocessing_training_tabula import MelanomaDataset
 
 
-directory = "/kaggle/input/siim-isic-melanoma-classification/train.csv"
+directory = "/kaggle/input/siim-isic-melanoma-classification/"
 dataset = MelanomaDataset(directory)
 dataset.process_and_save()
 
